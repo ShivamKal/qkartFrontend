31e82615f5e8c166366f8bf51b391db730efd9e5
CrioUser <crio-user@criodo.com>
2023-03-09 16:40:17 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -9,8 +9,9 @@ import {
   Link
 } from "react-router-dom";
 import Checkout from "./components/Checkout";
+import Thanks from "./components/Thanks";
 export const config = {
-  endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
+  endpoint: `https://qkart-frontend-vj56.onrender.com/api/v1`,
 };
 
 function App() {
@@ -29,6 +30,9 @@ function App() {
             </Route>
             <Route path="/checkout">
               <Checkout />
+            </Route>
+            <Route path="/thanks">
+              <Thanks />
             </Route>
           </Switch>
       </Router>
