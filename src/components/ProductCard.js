f23ba27f8874bccc57c0dabcf1f43075b49ad208
CrioUser <crio-user@criodo.com>
2023-03-09 14:26:20 +0530

    “ME_QKART_FRONTEND_V2_MODULE_PRODUCTS”

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -10,10 +10,35 @@ import {
 } from "@mui/material";
 import React from "react";
 import "./ProductCard.css";
-
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import  BasicRating from './MUI/Rating'
 const ProductCard = ({ product, handleAddToCart }) => {
+  console.log(product, handleAddToCart )
   return (
-    <Card className="card">
+    <Card 
+      //sx={{ maxWidth: 275 }}
+      >
+      <CardMedia
+        sx={{ height: 340 }}
+        image={product?.image}
+        title="green iguana"
+      />
+      <CardContent>
+      <Typography variant="h6" gutterBottom>
+        {product?.name}
+      </Typography>
+      <Typography variant="h6" margin="normal" >
+        <strong>{"$"+product?.cost}</strong>
+      </Typography>
+        {/* <Rating
+          name="simple-controlled"
+          value={product?.rating}
+        /> */}
+        <BasicRating rating={product?.rating} />
+      </CardContent>
+      <CardActions fullWidth>
+        <Button className="card-button-custom" fullWidth variant="contained"><AddShoppingCartIcon />ADD TO CART</Button>
+      </CardActions>
     </Card>
   );
 };
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,13 @@
 import { Search, SentimentDissatisfied } from "@mui/icons-material";
 import {
+  Autocomplete,
   CircularProgress,
   Grid,
+  IconButton,
   InputAdornment,
   TextField,
 } from "@mui/material";
+import { spacing } from '@mui/system';
 import { Box } from "@mui/system";
 import axios from "axios";
 import { useSnackbar } from "notistack";
@@ -12,14 +15,128 @@ import React, { useEffect, useState } from "react";
 import { config } from "../App";
 import Footer from "./Footer";
 import Header from "./Header";
+import ProductCard from "./ProductCard";
 import "./Products.css";
+import SearchIcon from '@mui/icons-material/Search';
+import CircularIndeterminate from "./MUI/Loading";
 const Products = () => {
+  const [products, setProducts] = useState({
+    loading: false,
+    product: []
+  });
+  const [debounceTimeout, setDebounceTimeout] = useState(setTimeout(() => {}, 500))
+  const { enqueueSnackbar } = useSnackbar()
+
+  const performAPICall = async () => {
+    setProducts(prevState => {
+      return {
+        ...prevState,
+        loading: true
+      }
+    })
+    try {
+       await axios
+       .get(`${config.endpoint}/products`)
+       .then((response)=> {
+        setProducts(prevState => {
+          return {
+            ...prevState,
+            product: response?.data,
+            loading: false
+          }
+        })
+       });
+    } catch (e) {
+      const errObj = {...e}
+      enqueueSnackbar(errObj?.response?.data?.message, {variant: "warning"})
+    }
+    setProducts(prevState => {
+      return {
+        ...prevState,
+       loading: false
+      }
+    })
+  };
+  useEffect(() => {
+    performAPICall();
+  },[]);
+
+  const performSearch = async (text) => {
+    setProducts(prevState => {
+      return {
+        ...prevState,
+        loading: true
+      }
+    })
+    try {
+       await axios
+       .get(`${config.endpoint}/products/search?value=${text}`)
+       .then((response)=> {
+        console.log("search -->", response?.data)
+        setProducts(prevState => {
+          return {
+            ...prevState,
+            product: response?.data
+          }
+        })
+       });
+    } catch (e) {
+      const error = {...e}
+      console.log("search -->", error)
+      if (error.response !== undefined && error.response.status === 404) {
+        setProducts((prevState) => ({
+          ...prevState,
+          product: error?.response?.data,
+        }));
+        enqueueSnackbar("No products found", { variant: "error" });
+      } else {
+        enqueueSnackbar(
+          "Something went wrong. Check the backend console for more details",
+          { variant: "error" }
+        );
+      }
+    }
+    setProducts(prevState => {
+      return {
+        ...prevState,
+       loading: false
+      }
+    })
+  };
+  const debounceSearch = (event, debounceTimeout) => {
+    clearTimeout(debounceTimeout)
+    setDebounceTimeout(
+      setTimeout(() => {
+        performSearch(event.target.value)
+      }, 500)
+    )
+  }
+  const handleAddToCart = () => {
+
+  }
   return (
     <div>
-      <Header>
-
-      </Header>
+      <Header></Header>
        <Grid container>
+        <TextField 
+          id="outlined-basic" 
+          placeholder="Search for items/categories" 
+          fullWidth 
+          variant="outlined" 
+          onChange={(e) => debounceSearch(e, debounceTimeout)}
+          InputProps={{
+            endAdornment: 
+            <InputAdornment position="end">
+            <IconButton
+              aria-label="toggle password visibility"
+              edge="end"
+            >
+              <SearchIcon />
+            </IconButton>
+          </InputAdornment>,
+          }}
+
+          />
          <Grid item className="product-grid">
            <Box className="hero">
              <p className="hero-heading">
@@ -29,6 +146,16 @@ const Products = () => {
            </Box>
          </Grid>
        </Grid>
+       <Grid container centered>
+           {!products?.loading ? (products?.product?.length > 0 ? products?.product?.map((product, index) => {
+            return (
+              <Grid key={index} item xs={6} md={3}sm={{margin: 2}} className="product-grid"><ProductCard product={product} handleAddToCart={handleAddToCart} /> </Grid>
+            )
+           })
+          :  <div className="loading-section" centered fullWidth>No Products Found</div>) : <div centered className="loading-section"><CircularIndeterminate />Loading Products</div>
+          }
+       </Grid>
+      
       <Footer />
     </div>
   );
